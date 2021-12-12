2692fafa3bcadcbbdc1d107f2af718f960b7216a
44lr <73897958+44lr@users.noreply.github.com>
2021-12-12 13:07:16 -0300

    added logger

---
+++ source/shared/util/log.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <fmt/format.h>
+
+enum class ELogType {
+	Verbose,
+	Info,
+	Warning,
+	Error
+};
+
+template <typename... Args>
+inline std::string FormatLog(const char* cFmt, Args&&... Arguments) {
+	return fmt::vformat(cFmt, fmt::make_format_args(std::forward<Args>(Arguments)...));
+}
+
+void Log(ELogType eLogType, std::string sMessage);
+
+template<typename... Args>
+inline void LogVerbose(const char* cFmt, Args... Arguments) { Log(ELogType::Verbose, FormatLog(cFmt, std::forward<Args>(Arguments)...)); }
+
+template<typename... Args>
+inline void LogInfo(const char* cFmt, Args... Arguments) { Log(ELogType::Info, FormatLog(cFmt, std::forward<Args>(Arguments)...)); }
+
+template<typename... Args>
+inline void LogWarning(const char* cFmt, Args... Arguments) { Log(ELogType::Warning, FormatLog(cFmt, std::forward<Args>(Arguments)...)); }
+
+template<typename... Args>
+inline void LogError(const char* cFmt, Args... Arguments) { Log(ELogType::Error, FormatLog(cFmt, std::forward<Args>(Arguments)...)); }
