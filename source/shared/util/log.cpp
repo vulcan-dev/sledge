bf7e30dd7698fb8cebbdfd5572aa9e73f4c1e3b8
44lr <73897958+44lr@users.noreply.github.com>
2021-12-12 14:12:22 -0300

    Update log.cpp

--- source/shared/util/log.cpp
+++ source/shared/util/log.cpp
@@ -1,30 +1,30 @@
-#include "log.h"
-#include <fmt/color.h>
+ #include "log.h"
+ #include <fmt/color.h>
 
-void Log(ELogType eLogType, std::string sText) {
-	fmt::color LogColor = fmt::color::white;
-	std::string sLogType;
+ void Log(ELogType eLogType, std::string sText) {
+ 	fmt::color LogColor = fmt::color::white;
+ 	std::string sLogType;
 
-	switch (eLogType)
-	{
-	case ELogType::Verbose:
-		sLogType = "VERBOSE";
-		LogColor = fmt::color::green;
-		break;
-	case ELogType::Info:
-		sLogType = "INFO";
-		LogColor = fmt::color::blue;
-		break;
-	case ELogType::Warning:
-		sLogType = "WARNING";
-		LogColor = fmt::color::yellow;
-		break;
-	case ELogType::Error:
-		sLogType = "ERROR";
-		LogColor = fmt::color::red;
-		break;
-	}
+ 	switch (eLogType)
+ 	{
+ 	case ELogType::Verbose:
+ 		sLogType = "VERBOSE";
+ 		LogColor = fmt::color::green;
+ 		break;
+ 	case ELogType::Info:
+ 		sLogType = "INFO";
+ 		LogColor = fmt::color::blue;
+ 		break;
+ 	case ELogType::Warning:
+ 		sLogType = "WARNING";
+ 		LogColor = fmt::color::yellow;
+ 		break;
+ 	case ELogType::Error:
+ 		sLogType = "ERROR";
+ 		LogColor = fmt::color::red;
+ 		break;
+ 	}
 
-	fmt::print(fg(LogColor), "[{}]", sLogType);
-	fmt::print(fg(fmt::color::white), " {}\n", sText);
-}
+ 	fmt::print(fg(LogColor), "[{}]", sLogType);
+ 	fmt::print(fg(fmt::color::white), " {}\n", sText);
+ }
