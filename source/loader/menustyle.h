7a1d964ff2cae3017de5d8302919d1b2111c6f50
44lr <73897958+44lr@users.noreply.github.com>
2021-12-14 11:35:55 -0300

    Update menustyle.h

--- source/loader/menustyle.h
+++ source/loader/menustyle.h
@@ -2,12 +2,20 @@
 
 #include <imgui.h>
 namespace MenuStyle {
+	namespace Colors {
+		static ImColor White(1.f, 1.f, 1.f, 1.f);
+		static ImColor Black(0.f, 0.f, 0.f);
+		static ImColor LighterBlue(.1f, .1f, 1.f);
+		static ImColor LightBlue(.1f, .1f, .75f);
+		static ImColor Blue(.1f, .1f, .5);
+		static ImColor DarkBlue(.05f, .05f, .25f);
+	}
+
 	inline void Apply() {
 		ImGuiStyle& Style = ImGui::GetStyle();
 
-		Style.WindowPadding = ImVec2(0.f, 0.f);
-		Style.WindowBorderSize = 0.f;
-
-		Style.Colors[ImGuiCol_Button] = ImVec4(.05f, .05f, .6f, 1.f);
+		Style.Colors[ImGuiCol_Button] = Colors::LightBlue;
+		Style.Colors[ImGuiCol_ButtonHovered] = Colors::LighterBlue;
+		Style.Colors[ImGuiCol_ButtonActive] = Colors::DarkBlue;
 	}
 }
