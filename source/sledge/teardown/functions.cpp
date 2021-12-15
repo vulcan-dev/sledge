ec00d003d8f7f8de3f857ad28fb9cbfab37fe8d0
44lr <73897958+44lr@users.noreply.github.com>
2021-12-15 20:50:53 -0300

    created functions.cpp and functions.h

---
+++ source/sledge/teardown/functions.cpp
@@ -0,0 +1,26 @@
+#include "teardown/functions.h"
+#include "teardown/signatures.h"
+#include "teardown/functions/memory.h"
+
+#include "util/memory.h"
+#include "util/log.h"
+
+struct SScanneable {
+	CSignature m_Sig;
+	void* m_Function;
+	const char* m_Name;
+};
+
+static const SScanneable aFunctions[] = {
+	// Memory
+	{ Signatures::Free, &Teardown::free, "Teardown::Free"},
+	{ Signatures::Alloc, &Teardown::alloc, "Teardown::Alloc"},
+};
+
+void Teardown::GetFunctionAddresses() {
+	for (const SScanneable &Scanneable : aFunctions) {
+		unsigned long long dwFuncAddr = Memory::dwFindPattern(Scanneable.m_Sig);
+		memcpy(Scanneable.m_Function, &dwFuncAddr, sizeof(void*));
+		LogVerbose("{} - {}", Scanneable.m_Name, reinterpret_cast<void*>(dwFuncAddr));
+	}
+}
---
+++ source/sledge/teardown/functions.h
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace Teardown {
+	void GetFunctionAddresses();
+}
