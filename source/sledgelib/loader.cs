8d9fccff13a917614ae201cab4c3687c652eda85
44lr <73897958+44lr@users.noreply.github.com>
2021-12-17 17:18:12 -0300

    organize sledgelib and loader

---
+++ source/sledgelib/loader.cs
@@ -0,0 +1,156 @@
+﻿using System.Text.Json;
+using System.Reflection;
+
+internal class SledgeLoader
+{
+    public delegate IntPtr GetInternalAPIDelegate();
+    public delegate bool InitDelegate(GetInternalAPIDelegate GetInternalAPI, string sModulePath);
+
+    public static string? m_ModulePath;
+    public static string? m_ModsPath;
+
+    private struct SAssemblyConfig
+    {
+        public string sTypeName {  get; set; }
+        public string sMethodName { get; set; }
+    }
+
+    /*
+        to-do:
+                (!) implement protection / restriction so modules can't do whatever they want
+                 -  improve error checking, some of this feels way too overexagerated, but maybe not, who knows
+    */
+    private static bool LoadAssembly(string sAssemblyName)
+    {
+        SledgeLib.WriteLog("Loading and initializing assembly: " + sAssemblyName);
+
+        string sConfig;
+        try
+        {
+            sConfig = System.IO.File.ReadAllText(m_ModsPath + sAssemblyName + ".info.json");
+        }
+        catch (Exception e)
+        {
+            SledgeLib.WriteError("Failed to open info.json: " + e);
+            return false;
+        }
+
+        SAssemblyConfig AssemblyConfig;
+        try
+        {
+            AssemblyConfig = JsonSerializer.Deserialize<SAssemblyConfig>(sConfig);
+        }
+        catch (Exception e)
+        {
+            SledgeLib.WriteError("error while deserializing config: " + e.ToString());
+            return false;
+        }
+
+        if (string.IsNullOrEmpty(AssemblyConfig.sTypeName) || string.IsNullOrEmpty(AssemblyConfig.sMethodName))
+        {
+            SledgeLib.WriteError("improperly formatted config.json (missing sTypeName and/or sMethodName)");
+            return false;
+        }
+
+        SledgeLib.WriteLog("Loaded assembly config: (sTypeName: " + AssemblyConfig.sTypeName + " | sMethodName: " + AssemblyConfig.sMethodName + ")");
+
+        Assembly vAssembly;
+        try
+        {
+            vAssembly = Assembly.Load(sAssemblyName);
+        }
+        catch (Exception e)
+        {
+            SledgeLib.WriteError("LoadAssembly failed: " + e);
+            return false;
+        }
+        SledgeLib.WriteLog("Loaded assembly: " + sAssemblyName);
+
+        Type? AssemblyType;
+        try
+        {
+            AssemblyType = vAssembly.GetType(AssemblyConfig.sTypeName, true, false);
+        }
+        catch (Exception e)
+        {
+            SledgeLib.WriteError("GetType failed: " + e);
+            return false;
+        }
+
+        if (AssemblyType == null)
+        {
+            SledgeLib.WriteError("AssemblyType was null");
+            return false;
+        }
+
+        SledgeLib.WriteLog("Got assembly type: " + AssemblyType.ToString());
+
+        MethodInfo? AssemblyMethod;
+        try
+        {
+            AssemblyMethod = AssemblyType.GetMethod(AssemblyConfig.sMethodName);
+        }
+        catch (System.Exception e)
+        {
+            SledgeLib.WriteError("GetMethod failed: " + e);
+            return false;
+        }
+
+        if (AssemblyMethod == null)
+        {
+            SledgeLib.WriteError("AssemblyMethod was null");
+            return false;
+        }
+
+        SledgeLib.WriteLog("Got assembly method: " + AssemblyMethod.ToString());
+
+        object? AssemblyInstance;
+        try
+        {
+            AssemblyInstance = Activator.CreateInstance(AssemblyType);
+        }
+        catch (System.Exception e)
+        {
+            SledgeLib.WriteError("CreateInstance error:" + e);
+            return false;
+        }
+
+        if (AssemblyInstance == null)
+        {
+            SledgeLib.WriteError("AssemblyInstance was null");
+            return false;
+        }
+
+        SledgeLib.WriteLog("Got assembly instance: " + AssemblyInstance.ToString());
+
+        try
+        {
+            AssemblyMethod.Invoke(AssemblyInstance, null);
+        }
+        catch (Exception e)
+        {
+            SledgeLib.WriteError("Method.Invoke error:" + e);
+            return false;
+        }
+
+        SledgeLib.WriteLog("Fully loaded assembly: " + sAssemblyName);
+
+        return true;
+    }
+
+    public static bool Init(GetInternalAPIDelegate GetInternalAPI, string sModulePath)
+    {
+        IntPtr pInternal = GetInternalAPI();
+        if (!SledgeLib._SetInternal(pInternal))
+            return false;
+
+        SledgeLib.WriteLog("loading mods");
+
+        m_ModulePath = sModulePath;
+        m_ModsPath = sModulePath + "\\mods\\";
+
+        LoadAssembly("sledge_examplemod");
+
+        return true;
+    }
+}
--- source/sledgelib/sledgelib.cs
+++ source/sledgelib/sledgelib.cs
@@ -1,186 +1,41 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-using System.Text.Json;
-
-public delegate void WriteLogDelegate(string cMsg, params object[] args);
-public struct CSledgeInternal
+﻿public class SledgeLib
 {
-    public WriteLogDelegate WriteLog;
-    public WriteLogDelegate WriteError;
-}
-
-struct SAssemblyConfig
-{
-    public string sTypeName { get; set; }
-    public string sMethodName { get; set; } 
-}
-
-class SledgeLoader
-{
-    public delegate IntPtr GetInternalAPIDelegate();
-    public delegate bool InitDelegate(GetInternalAPIDelegate GetInternalAPI, string sModulePath);
-
-    public static string? m_ModulePath;
-    public static string? m_ModsPath;
-
-    /*
-        to-do: improve error checking, some of this feels way too overexagerated, but maybe not, who knows
-    */
-    public static bool LoadAssembly(string sAssemblyName)
+    private enum ELogType
     {
-        SledgeLib.WriteLog("Loading and initializing assembly: " + sAssemblyName);
-
-        string sConfig;
-        try
-        {
-            sConfig = System.IO.File.ReadAllText(m_ModsPath + sAssemblyName + ".info.json");
-        } catch(Exception e)
-        {
-            SledgeLib.WriteError("Failed to open info.json: " + e);
-            return false;
-        }
-
-        SAssemblyConfig AssemblyConfig;
-        try
-        {
-            AssemblyConfig = JsonSerializer.Deserialize<SAssemblyConfig>(sConfig);
-        }
-        catch(Exception e)
-        {
-            SledgeLib.WriteError("error while deserializing config: " + e.ToString());
-            return false;
-        }
-
-        if (string.IsNullOrEmpty(AssemblyConfig.sTypeName) || string.IsNullOrEmpty(AssemblyConfig.sMethodName))
-        {
-            SledgeLib.WriteError("improperly formatted config.json (missing sTypeName and/or sMethodName)");
-            return false;
-        }
-
-        SledgeLib.WriteLog("Loaded assembly config: (sTypeName: " + AssemblyConfig.sTypeName + " | sMethodName: " + AssemblyConfig.sMethodName + ")");
-
-        Assembly vAssembly;
-        try
-        {
-            vAssembly = Assembly.Load(sAssemblyName);
-        } catch(Exception e)
-        {
-            SledgeLib.WriteError("LoadAssembly failed: " + e);
-            return false;
-        }
-        SledgeLib.WriteLog("Loaded assembly: " + sAssemblyName);
-
-        Type? AssemblyType;
-        try
-        {
-            AssemblyType = vAssembly.GetType(AssemblyConfig.sTypeName, true, false);
-        } catch(Exception e)
-        {
-            SledgeLib.WriteError("GetType failed: " + e);
-            return false;
-        }
-
-        if (AssemblyType == null)
-        {
-            SledgeLib.WriteError("AssemblyType was null");
-            return false;
-        }
-
-        SledgeLib.WriteLog("Got assembly type: " + AssemblyType.ToString());
-
-        MethodInfo? AssemblyMethod;
-        try
-        {
-            AssemblyMethod = AssemblyType.GetMethod(AssemblyConfig.sMethodName);
-        } catch (System.Exception e)
-        {
-            SledgeLib.WriteError("GetMethod failed: " + e);
-            return false;
-        }
-
-        if (AssemblyMethod == null)
-        {
-            SledgeLib.WriteError("AssemblyMethod was null");
-            return false;
-        }
-
-        SledgeLib.WriteLog("Got assembly method: " + AssemblyMethod.ToString());
-
-        object? AssemblyInstance;
-        try
-        {
-            AssemblyInstance = Activator.CreateInstance(AssemblyType);
-        } catch(System.Exception e)
-        {
-            SledgeLib.WriteError("CreateInstance error:" + e);
-            return false;
-        }
-
-        if (AssemblyInstance == null)
-        {
-            SledgeLib.WriteError("AssemblyInstance was null");
-            return false;
-        }
-
-        SledgeLib.WriteLog("Got assembly instance: " + AssemblyInstance.ToString());
-
-        try
-        {
-            AssemblyMethod.Invoke(AssemblyInstance, null);
-        } catch(Exception e)
-        {
-            SledgeLib.WriteError("Method.Invoke error:" + e);
-            return false;
-        }
-
-        SledgeLib.WriteLog("Fully loaded assembly: " + sAssemblyName);
-
-        return true;
+        General = 4,
+        Error = 5
     }
 
-    public static bool Init(GetInternalAPIDelegate GetInternalAPI, string sModulePath)
+    private struct CSledgeInternal
     {
-        m_ModulePath = sModulePath;
-        m_ModsPath = sModulePath + "\\mods\\";
-
-        IntPtr pInternal = GetInternalAPI();
-
-        CSledgeInternal Internal = (CSledgeInternal)System.Runtime.InteropServices.Marshal.PtrToStructure(pInternal, typeof(CSledgeInternal));
-
-        SledgeLib._SetInternal(Internal);
-
-        Internal.WriteLog("sledgelib loaded");
-
-        try
-        {
-            LoadAssembly("sledge_examplemod");
-        } catch (Exception e)
-        {
-            Internal.WriteLog("wtf:" + e);
-        }
-
-
-        return true;
+        public delegate void LogDelegate(ELogType LogType, string sMsg);
+        public LogDelegate Log;
     }
-}
 
-public class SledgeLib
-{
     private static CSledgeInternal _m_Internal;
 
-    public static void _SetInternal(CSledgeInternal Internal)
+    internal static bool _SetInternal(IntPtr pInternal)
     {
-        _m_Internal = Internal;
+        if (pInternal == IntPtr.Zero)
+            return false;
+
+        object? InternalObj = System.Runtime.InteropServices.Marshal.PtrToStructure(pInternal, typeof(CSledgeInternal));
+        if (InternalObj == null)
+            return false;
+
+        _m_Internal = (CSledgeInternal)InternalObj;
+        WriteLog("sledgelib internal api loaded");
+
+        return true;
     }
 
     public static void WriteLog(string sLog)
     {
-        _m_Internal.WriteLog(sLog);
+        _m_Internal.Log(ELogType.General, sLog);
     }
 
     public static void WriteError(string sLog)
     {
-        _m_Internal.WriteError(sLog);
+        _m_Internal.Log(ELogType.Error, sLog);
     }
 }
