a371e9cfec55ce167916e35b00303674e021c987
44lr <73897958+44lr@users.noreply.github.com>
2021-12-17 21:01:15 -0300

    Create binds.cs

---
+++ source/sledgelib/binds.cs
@@ -0,0 +1,54 @@
+﻿public delegate void BindCallback();
+
+internal struct SBindWrapper
+{
+    IntPtr m_BindInstance;
+
+    internal delegate void DestroyDelegate(SBindWrapper pThis);
+    internal DestroyDelegate Destroy;
+
+    internal delegate void UpdateActiveDelegate(SBindWrapper pThis, bool bActive);
+    internal UpdateActiveDelegate UpdateActive;
+}
+
+/*
+    to-do: port over the other bind types
+ */
+public enum EBindType : uint
+{
+    Callback = 0
+}
+
+public class CBind
+{
+    private SBindWrapper m_BindWrapper;
+
+    public CBind(EBindType eType, EKeyCode iKeyCode, BindCallback pCallback, bool bActive = true)
+    {
+        SledgeLib.WriteLog("registering bind");
+        IntPtr pBindWrapper = SledgeLib.m_Internal.CreateBind(eType, iKeyCode, pCallback, bActive); ;
+
+        if (pBindWrapper == IntPtr.Zero)
+            return;
+
+        SBindWrapper? NewBindWrapper = (SBindWrapper)System.Runtime.InteropServices.Marshal.PtrToStructure(pBindWrapper, typeof(SBindWrapper));
+        if (NewBindWrapper == null)
+            return;
+
+        m_BindWrapper = (SBindWrapper)NewBindWrapper;
+
+        SledgeLib.WriteLog("bind registered");
+    }
+
+    ~CBind()
+    {
+        SledgeLib.WriteLog("destroying bind");
+        this.m_BindWrapper.Destroy(this.m_BindWrapper);
+        SledgeLib.WriteLog("bind destroyed");
+    }
+
+    public void UpdateActive(bool bActive)
+    {
+        this.m_BindWrapper.UpdateActive(this.m_BindWrapper, bActive);
+    }
+}
