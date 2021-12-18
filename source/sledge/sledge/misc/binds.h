817d31e2fc4adff45547a8c1b277170f7bbf0826
44lr <73897958+44lr@users.noreply.github.com>
2021-12-17 21:03:59 -0300

    Update binds.h

--- source/sledge/sledge/misc/binds.h
+++ source/sledge/sledge/misc/binds.h
@@ -1,6 +1,6 @@
 #include "util/keycodes.h"
 
-enum class EBindType : unsigned char {
+enum class EBindType : unsigned int {
 	Callback,
 	Increase,
 	Decrease,
@@ -10,15 +10,18 @@ enum class EBindType : unsigned char {
 
 class CKeyBind {
 public:
-	CKeyBind(EBindType eType, int iKeyCode, void* pValue, bool bActive = true);
-	~CKeyBind();
-	void OnKeyDown();
-	void OnKeyUp();
-
 	EBindType m_BindType;
 	unsigned __int64 m_KeyId;
 	bool m_Active;
 	void* m_Value;
+
+	CKeyBind() = delete;
+	CKeyBind(EBindType eType, int iKeyCode, void* pValue, bool bActive = true);
+
+	~CKeyBind();
+	void OnKeyDown();
+	void OnKeyUp();
+
 };
 
 namespace Binds {
