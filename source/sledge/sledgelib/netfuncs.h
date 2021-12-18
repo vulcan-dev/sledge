166f0ff2ad5bdf66b3cb93da4dd518fcd2fd1f95
44lr <73897958+44lr@users.noreply.github.com>
2021-12-17 22:56:40 -0300

    update lib funcs

--- source/sledge/sledgelib/netfuncs.h
+++ source/sledge/sledgelib/netfuncs.h
@@ -8,8 +8,18 @@ namespace SledgeLib {
 		void WriteLog(int eLogType, char* cMsg);
 
 		/*
-			internal delegate IntPtr CreateBindDelegate(EBindType eType, EKeyCode iKeyCode, BindCallback pCallback, bool bActive = true);
+			internal delegate IntPtr CreateBindDelegate(EBindType eType, EKeyCode iKeyCode, CallbackDelegate pCallback, bool bActive = true);
 		*/
 		void* CreateBind(unsigned int eType, int iKeyCode, void* pCallback, bool bActive);
+
+		/*
+			internal delegate IntPtr CreateCallbackDelegate(ECallbackType eType, CallbackDelegate pCallback, bool bActive = true);
+		*/
+		void* CreateCallback(unsigned int eType, void* pCallback, bool bActive);
+
+		/*
+			internal delegate bool IsPlayingDelegate();
+		*/
+		bool IsPlaying();
 	}
 }
