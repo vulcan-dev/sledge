e8e0f86babfd46d5a540b0dbbf299a748678920c
44lr <73897958+44lr@users.noreply.github.com>
2021-12-18 22:59:08 -0300

    Create math.cs

---
+++ source/sledgelib/math.cs
@@ -0,0 +1,20 @@
+﻿public struct Vector2
+{
+    public float x;
+    public float y;
+}
+
+public struct Vector3
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public struct Vector4
+{
+    public float x;
+    public float y;
+    public float z;
+    public float w;
+}
