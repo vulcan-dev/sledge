311f381ea5121d707c5e3a5e7dcd8bddec22f639
44lr <73897958+44lr@users.noreply.github.com>
2021-12-18 22:59:11 -0300

    Create player.cs

---
+++ source/sledgelib/player.cs
@@ -0,0 +1,44 @@
+﻿internal struct SPlayerWrapper
+{
+    internal delegate float GetHealthDelegate();
+    internal GetHealthDelegate GetHealth;
+
+    internal delegate void SetHealthDelegate(float fHealth);
+    internal SetHealthDelegate SetHealth;
+
+    internal delegate Vector3 GetPositionDelegate();
+    internal GetPositionDelegate GetPosition;
+
+    internal delegate void SetPositionDelegate(Vector3 vPosition);
+    internal SetPositionDelegate SetPosition;
+}
+
+public interface Player
+{
+    internal static SPlayerWrapper m_PlayerWrapper;
+
+    internal static void SetWrapper(IntPtr pPlayerWrapper)
+    {
+        if (pPlayerWrapper == IntPtr.Zero)
+            return;
+
+        SPlayerWrapper? NewPlayerWrapper = (SPlayerWrapper?)System.Runtime.InteropServices.Marshal.PtrToStructure(pPlayerWrapper, typeof(SPlayerWrapper));
+        
+        if (NewPlayerWrapper == null)
+            return;
+
+        m_PlayerWrapper = (SPlayerWrapper)NewPlayerWrapper;
+    }
+
+    public static float m_Health
+    {
+        get { return m_PlayerWrapper.GetHealth(); }
+        set { m_PlayerWrapper.SetHealth(value);}
+    }
+
+    public static Vector3 m_Position
+    {
+        get {  return m_PlayerWrapper.GetPosition(); }
+        set { m_PlayerWrapper.SetPosition(value); }
+    }
+}
