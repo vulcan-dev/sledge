9daa369f17853b278bdb101aeb3d98e3a23e05fb
44lr <73897958+44lr@users.noreply.github.com>
2021-12-30 01:33:35 -0300

    remove left over code from testing

--- source/sledge/sledgelib/netfuncs/registry.cpp
+++ source/sledge/sledgelib/netfuncs/registry.cpp
@@ -12,8 +12,4 @@ sledgelib_func void SetInt(char* cKeyName, int iValue) { Teardown::SetInt(g_Game
 sledgelib_func int GetInt(char* cKeyName) { return Teardown::GetInt(g_Game->m_Registry, new small_string(cKeyName)); }
 
 sledgelib_func void SetFloat(char* cKeyName, float fValue) { Teardown::SetFloat(g_Game->m_Registry, new small_string(cKeyName), fValue); }
-sledgelib_func float GetFloat(char* cKeyName) {
-	small_string ssKeyName(cKeyName);
-	float fValue = Teardown::GetFloat(g_Game->m_Registry, &ssKeyName);
-	return fValue;
-}
+sledgelib_func float GetFloat(char* cKeyName) { return Teardown::GetFloat(g_Game->m_Registry, new small_string(cKeyName)); }
