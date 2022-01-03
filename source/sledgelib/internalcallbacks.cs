1a8d0ade8252f4c73a633ec5e30d59e065047bda
44lr <73897958+44lr@users.noreply.github.com>
2022-01-03 11:02:27 -0300

    add internal callbacks

---
+++ source/sledgelib/internalcallbacks.cs
@@ -0,0 +1,15 @@
+﻿namespace SledgeLib
+{
+    internal class InternalCallbacks
+    {
+        internal static dUIntCallback cb_StateChangeFunc = new dUIntCallback((uint iState) =>
+        {
+            if (iState == (uint)EGameState.Playing)
+            {
+                Scene._ResetSounds();
+            }
+        });
+
+        internal static CCallback? cb_StateChange;
+    }
+}
