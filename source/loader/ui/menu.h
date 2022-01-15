876878b0d636f0c6153cc6fe56f872c80665ef04
vulcan-dev <vitexuhd@gmail.com>
2022-01-15 17:12:24 +0000

    Added Alpha to FromRGB

--- source/loader/ui/menu.h
+++ source/loader/ui/menu.h
@@ -13,13 +13,13 @@ namespace Menu {
 		inline ImColor Blue			(.1f, .1f, .5);
 		inline ImColor DarkBlue		(.05f, .05f, .25f);
 
-		inline ImColor FromRGB(const float& r, const float& g, const float& b) {
+		inline ImColor FromRGB(const float& r, const float& g, const float& b, const float& a = 255) {
 			ImColor col;
 
 			col.Value.x = r / 255;
 			col.Value.y = g / 255;
 			col.Value.z = b / 255;
-			col.Value.w = 1;
+			col.Value.w = a / 255;
 
 			return col;
 		}
