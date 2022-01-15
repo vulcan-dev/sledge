f01e7128b83225ae4ae9ad263f6b216598422504
vulcan-dev <vitexuhd@gmail.com>
2022-01-15 16:19:20 +0000

    Fix crash with strcmp

--- source/sledge/sledge/hooks/createwindow.cpp
+++ source/sledge/sledge/hooks/createwindow.cpp
@@ -13,12 +13,12 @@ LRESULT CBTProc(_In_ int nCode, _In_ WPARAM wParam, _In_ LPARAM lParam) {
 	if (nCode != HCBT_CREATEWND)
 		return CallNextHookEx(Hook, nCode, wParam, lParam);
 
-	if (nCode == HCBT_CREATEWND) {
+	if (nCode == HCBT_CREATEWND && !g_WindowReady) {
 		CBT_CREATEWNDA* CWArguments = reinterpret_cast<CBT_CREATEWNDA*>(lParam);
 		if (!CWArguments)
 			return CallNextHookEx(Hook, nCode, wParam, lParam);
-
-		if (!strcmp(CWArguments->lpcs->lpszName, "Teardown")) {
+		
+		if (strcmp(CWArguments->lpcs->lpszName, "Teardown")) {
 			g_hWnd = reinterpret_cast<HWND>(wParam);;
 			g_WindowWidth = CWArguments->lpcs->cx - 16;
 			g_WindowHeight = CWArguments->lpcs->cy - 39;
