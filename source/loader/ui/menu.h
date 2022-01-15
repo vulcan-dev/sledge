e5d8314ef77858e85b4b346ca57fc69de69940cd
vulcan-dev <vitexuhd@gmail.com>
2022-01-15 17:11:34 +0000

    Added FromRGB Function

--- source/loader/ui/menu.h
+++ source/loader/ui/menu.h
@@ -12,5 +12,16 @@ namespace Menu {
 		inline ImColor LightBlue	(.1f, .1f, .75f);
 		inline ImColor Blue			(.1f, .1f, .5);
 		inline ImColor DarkBlue		(.05f, .05f, .25f);
+
+		inline ImColor FromRGB(const float& r, const float& g, const float& b) {
+			ImColor col;
+
+			col.Value.x = r / 255;
+			col.Value.y = g / 255;
+			col.Value.z = b / 255;
+			col.Value.w = 1;
+
+			return col;
+		}
     }
 }
