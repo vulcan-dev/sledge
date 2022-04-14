d18e85fb01f8a615d15479e29cd4d6c1a444dff4
44lr <73897958+44lr@users.noreply.github.com>
2022-04-14 15:23:56 -0300

    run patch every time a project is created

--- Create project.bat
+++ Create project.bat
@@ -1,6 +1,7 @@
 @echo off
 mkdir proj
 cd proj
-cmake ../
+cmake ../ -A x64 -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_VS_PLATFORM_TOOLSET_HOST_ARCHITECTURE=x64
 cd ..
+powershell -ExecutionPolicy Unrestricted -File "./scripts/patchproj.ps1" "./proj" -sln -vcx -cs -zc 
 pause
