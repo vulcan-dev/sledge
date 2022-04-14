b1667715d9ec2ea820380086608151a6c8513507
44lr <73897958+44lr@users.noreply.github.com>
2022-04-14 15:30:58 -0300

    add C# func _WriteLog

---
+++ source/sledge_core/net/functions/misc.cpp
@@ -0,0 +1,5 @@
+#include "util/log.h"
+
+#define sledgelib_func extern "C" __declspec(dllexport)
+
+sledgelib_func void _WriteLog(int eLogType, char* cMsg) { Log(static_cast<ELogType>(eLogType), cMsg); }
