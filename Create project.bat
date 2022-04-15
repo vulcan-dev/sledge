b244680afca64676c61e4d4be89dd8644beb5d96
44lr <73897958+44lr@users.noreply.github.com>
2022-04-15 11:20:22 -0300

    add ChildProcessDbgSettings

--- Create project.bat
+++ Create project.bat
@@ -3,5 +3,6 @@ mkdir proj
 cd proj
 cmake ../ -A x64 -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_VS_PLATFORM_TOOLSET_HOST_ARCHITECTURE=x64
 cd ..
+echo f | xcopy /y /f "./scripts/sledge.ChildProcessDbgSettings" "./proj/sledge.ChildProcessDbgSettings"
 powershell -ExecutionPolicy Unrestricted -File "./scripts/patchproj.ps1" "./proj" -sln -vcx -cs -zc 
 pause
