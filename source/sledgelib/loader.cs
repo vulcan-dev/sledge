689dff609020cdfbcd6f8f360c41cf384d908ecb
44lr <73897958+44lr@users.noreply.github.com>
2022-04-15 14:16:18 -0300

    Update loader.cs

--- source/sledgelib/loader.cs
+++ source/sledgelib/loader.cs
@@ -1,38 +1,17 @@
-﻿using SledgeLib;
+using SledgeLib;
 
-internal class CSledgeLoader
+internal class Loader
 {
+    /*
+     * function that gets called from sledge_core once it's injected
+     */
     public delegate bool dInit();
-
     public static bool Init()
     {
-        Log.Verbose("Initializing binds");
-        CBindManager.RegisterInputReader(CBindManager.InputReader);
+        Log.General("Initializing mod manager");
+        try { ModManager.Init(); } catch (System.Exception e) { Log.Error("Error while initializing the mod manager: {0}", e); }
 
-        Log.Verbose("Initializing callbacks");
-        CCallbackManager.RegisterCallback(ECallbackType.PlayerSpawn, CCallbackManager.PlayerSpawnCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.PreUpdate, CCallbackManager.PreUpdateCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.PostUpdate, CCallbackManager.PostUpdateCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.PrePlayerUpdate, CCallbackManager.PrePlayerUpdateCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.PostPlayerUpdate, CCallbackManager.PostPlayerUpdateCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.StateChange, CCallbackManager.StateChangeCallback);
-        CCallbackManager.RegisterCallback(ECallbackType.Tick, CCallbackManager.TickCallback);
-
-        Log.Verbose("Registering internal callbacks");
-        InternalCallbacks.cb_StateChange = new CCallback(ECallbackType.StateChange, InternalCallbacks.cb_StateChangeFunc);
-
-        Log.Verbose("Loading mods");
-        try
-        {
-            CModLoader.Init();
-        }
-        catch (Exception e)
-        {
-            Log.General("CModLoader.Init() failed: {0}", e.Message);
-            return false;
-        }
-
-        Log.General("Sledgelib succesfully initialized");
+        Log.General("Sledgelib successfully loaded");
         return true;
     }
 }
