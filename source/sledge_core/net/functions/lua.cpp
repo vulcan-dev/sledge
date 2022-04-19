25c316ed3fc2c270ef49a623b7c185f17910bd3b
44lr <73897958+44lr@users.noreply.github.com>
2022-04-19 15:15:27 -0300

    add C# bindings for lua functions, add _RegisterLuaFunctionInternal

---
+++ source/sledge_core/net/functions/lua.cpp
@@ -0,0 +1,23 @@
+#include "sledge/luahelpers.h"
+#include "net/sledgelib.h"
+#include "teardown/functions/lua.h"
+
+#define sledgelib_func extern "C" __declspec(dllexport)
+
+sledgelib_func void _RegisterLuaFunctionInternal(const char* cFunctionName, void* pFunction) { Sledge::LuaHelpers::AddToRegisteredLuaFunctions(cFunctionName, pFunction); }
+
+sledgelib_func int _lua_tointeger(lua_State* L, int iIndex) { return Teardown::lua_tointeger(L, iIndex); }
+sledgelib_func bool _lua_toboolean(lua_State* L, int iIndex) { return Teardown::lua_toboolean(L, iIndex); }
+
+sledgelib_func void* _lua_tolstring(lua_State* L, int iIndex) {
+	size_t lLength = 0;
+	const char* cLuaString = Teardown::lua_tolstring(L, iIndex, &lLength);
+	void* pStringBuilder = SledgeLib::Interface->AllocateString(lLength + 1);
+	
+	if (cLuaString)
+		memcpy(pStringBuilder, cLuaString, lLength);
+	else
+		memset(pStringBuilder, '\0', 1);
+
+	return pStringBuilder;
+}
