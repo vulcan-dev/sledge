ed8735df8cee562ee135a797b3f6d57da59e669d
44lr <73897958+44lr@users.noreply.github.com>
2022-04-24 16:20:38 -0300

    add misc and math types

---
+++ source/sledge_core/teardown/types/misc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "teardown/types/small_vector.h"
+
+struct SRaycastFilter
+{
+	int m_Mask;
+	int m_Layers;
+	char field_8[4];
+	bool m_IgnoreTransparent;
+	small_vector<class CBody*> m_IgnoredBodies;
+	char field_1D[32];
+	small_vector<class CEntity*> m_IgnoredShapes;
+};
