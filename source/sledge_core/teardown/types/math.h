439fe1646c950c17c154a155623816d2b54bbaa6
44lr <73897958+44lr@users.noreply.github.com>
2022-04-25 16:49:44 -0300

    add Bounds, change packing

--- source/sledge_core/teardown/types/math.h
+++ source/sledge_core/teardown/types/math.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#pragma pack(push, 1)
 struct Vector2 { float x, y; };
 struct Vector3 { float x, y, z; };
 struct Vector4 { float x, y, z, w; };
@@ -10,3 +11,9 @@ public:
 	Vector3 m_Position;
 	Vector4 m_Rotation;
 };
+
+struct Bounds {
+	Vector3 m_Min;
+	Vector3 m_Max;
+};
+#pragma pack(pop)
