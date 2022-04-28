afc4012d49022cf11125844d227736508c89e3e2
44lr <73897958+44lr@users.noreply.github.com>
2022-04-28 10:53:44 -0300

    change func sig

--- source/sledge_core/teardown/functions/scene.h
+++ source/sledge_core/teardown/functions/scene.h
@@ -6,7 +6,7 @@ class Shape;
 
 namespace Teardown {
 	inline bool (*QueryRaycast) (Scene* pScene, Vector3* vOrigin, Vector3* vDirection, float fMaxDist, SQueryFilter* pFilter, float* fDistOut, Vector3* vHitPos, Shape** pHitShape, void* pUnk);
-	inline bool (*QueryClosestPoint) (Scene* pScene, Vector3* vOrigin, Vector3* vDirection, float fMaskDist, SQueryFilter* pFilter, Vector3** vHitPos, Vector3** vHitNormal, Shape** pHitShape, void* pUnk);
+	inline bool (*QueryClosestPoint) (Scene* pScene, Vector3* vOrigin, float fMaskDist, SQueryFilter* pFilter, Vector3* vHitPos, Vector3* vHitNormal, Shape** pHitShape, void* pUnk);
 
 	inline bool (*MakeHole) (void* pUnknown, void* pUnknown2, Vector3* vPosition, float fSoftRadius, float fMidRadius, float fHardRadius, bool bSilent, void* pUnknown3);
 }
