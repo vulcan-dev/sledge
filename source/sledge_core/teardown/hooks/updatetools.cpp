150000e4427dd7fae8e3f427a80a6f1213c93658
44lr <73897958+44lr@users.noreply.github.com>
2022-04-28 09:51:46 -0300

    add UpdateTool hook

---
+++ source/sledge_core/teardown/hooks/updatetools.cpp
@@ -0,0 +1,33 @@
+#include "teardown/hooks.h"
+#include "teardown/offsets.h"
+
+#include "globals.h"
+
+#include <windef.h>
+#include <processthreadsapi.h>
+#include <detours.h>
+
+typedef void (*tUpdateToolBodies) (void*, float);
+tUpdateToolBodies _UpdateToolBodies;
+
+void hUpdateToolBodies(void*, float) {
+
+}
+
+void Teardown::Hooks::UpdateTools::Hook() {
+	_UpdateToolBodies = reinterpret_cast<tUpdateToolBodies>(g_BaseAddress + g_Offsets["UpdateToolBodies"]);
+
+	LogVerbose("UpdateToolBodies: {}", reinterpret_cast<void*>(_UpdateToolBodies));
+
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	DetourAttach(&_UpdateToolBodies, hUpdateToolBodies);
+	DetourTransactionCommit();
+}
+
+void Teardown::Hooks::UpdateTools::Unhook() {
+	DetourTransactionBegin();
+	DetourUpdateThread(GetCurrentThread());
+	DetourDetach(&_UpdateToolBodies, hUpdateToolBodies);
+	DetourTransactionCommit();
+}
