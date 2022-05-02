2d1d82a6cb671599c6155a068bcdf83e70f7f2e0
44lr <73897958+44lr@users.noreply.github.com>
2022-05-02 17:45:33 -0300

    check if the player is in game before allowing the execution of functions

--- source/sledgelib/public/scene.cs
+++ source/sledgelib/public/scene.cs
@@ -5,12 +5,68 @@ namespace SledgeLib
 {
     public class Scene
     {
-        [DllImport("sledge_core.dll")] public static extern QueryInfo QueryRaycast(Vector3 Origin, Vector3 Direction, float MaxDist);
-        [DllImport("sledge_core.dll")] public static extern QueryInfo QuerySpherecast(Vector3 Origin, Vector3 Direction, float Radius, float MaxDist);
-        [DllImport("sledge_core.dll")] public static extern QueryInfo QueryClosestPoint(Vector3 Origin, float MaxDist);
-        [DllImport("sledge_core.dll")] public static extern uint MakeHole(Vector3 Position, float SoftRadius, float MidRadius, float HardRadius, bool Silent);
-        [DllImport("sledge_core.dll")] public static extern void DrawLine(Vector3 Origin, Vector3 Goal, Vector4 Colour, bool UseDepth);
-        [DllImport("sledge_core.dll")] public static extern void DrawCross(Vector3 Position, Vector4 Colour, float Size);
-        [DllImport("sledge_core.dll")] public static extern void Paint(Vector3 Position, EPaintType Type, float Radius, float Probability);
+        [DllImport("sledge_core.dll")] internal static extern QueryInfo _QueryRaycast(Vector3 Origin, Vector3 Direction, float MaxDist);
+        [DllImport("sledge_core.dll")] internal static extern QueryInfo _QuerySpherecast(Vector3 Origin, Vector3 Direction, float Radius, float MaxDist);
+        [DllImport("sledge_core.dll")] internal static extern QueryInfo _QueryClosestPoint(Vector3 Origin, float MaxDist);
+        [DllImport("sledge_core.dll")] internal static extern uint _MakeHole(Vector3 Position, float SoftRadius, float MidRadius, float HardRadius, bool Silent);
+        [DllImport("sledge_core.dll")] internal static extern void _DrawLine(Vector3 Origin, Vector3 Goal, Vector4 Colour, bool UseDepth);
+        [DllImport("sledge_core.dll")] internal static extern void _DrawCross(Vector3 Position, Vector4 Colour, float Size);
+        [DllImport("sledge_core.dll")] internal static extern void _Paint(Vector3 Position, EPaintType Type, float Radius, float Probability);
+
+        public static QueryInfo QueryRaycast(Vector3 Origin, Vector3 Direction, float MaxDist)
+        {
+            if (Game.State != EGameState.Playing)
+                return new QueryInfo();
+
+            return _QueryRaycast(Origin, Direction, MaxDist);
+        }
+
+        public static QueryInfo QuerySpherecast(Vector3 Origin, Vector3 Direction, float Radius, float MaxDist)
+        {
+            if (Game.State != EGameState.Playing)
+                return new QueryInfo();
+
+            return _QuerySpherecast(Origin, Direction, Radius, MaxDist);
+        }
+
+        public static QueryInfo QueryClosestPoint(Vector3 Origin, float MaxDist)
+        {
+            if (Game.State != EGameState.Playing)
+                return new QueryInfo();
+
+            return _QueryClosestPoint(Origin, MaxDist);
+        }
+
+        public static uint MakeHole(Vector3 Position, float SoftRadius, float MidRadius, float HardRadius, bool Silent)
+        {
+            if (Game.State != EGameState.Playing)
+                return 0;
+
+            return _MakeHole(Position, SoftRadius, MidRadius, HardRadius, Silent);
+        }
+
+        public static void DrawLine(Vector3 Origin, Vector3 Goal, Vector4 Colour, bool UseDepth)
+        {
+            if (Game.State != EGameState.Playing)
+                return;
+
+            _DrawLine(Origin, Goal, Colour, UseDepth);
+        }
+
+        public static void DrawCross(Vector3 Position, Vector4 Colour, float Size)
+        {
+            if (Game.State != EGameState.Playing)
+                return;
+
+            _DrawCross(Position, Colour, Size);
+        }
+
+        public static void Paint(Vector3 Position, EPaintType Type, float Radius, float Probability)
+        {
+            if (Game.State != EGameState.Playing)
+                return;
+
+            _Paint(Position, Type, Radius, Probability);
+        }
     }
 }
