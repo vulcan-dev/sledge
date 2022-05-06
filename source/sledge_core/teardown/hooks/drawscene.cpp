6237276c58c626114175b102c96ced620918180e
44lr <73897958+44lr@users.noreply.github.com>
2022-05-05 22:28:02 -0300

    remove leftover check from debugging

--- source/sledge_core/teardown/hooks/drawscene.cpp
+++ source/sledge_core/teardown/hooks/drawscene.cpp
@@ -27,39 +27,34 @@ tDrawScene _DrawScene;
 #include "teardown/classes/renderer.h"
 #include "teardown/classes/player.h"
 void hDrawScene(Renderer* pRenderer, unsigned int a2, unsigned int /*iWidth*/, unsigned int /*iHeight*/, glm::mat4* /*mProjection*/, glm::mat4* /*mView*/) {
+	glm::mat4 PositionMatrix = glm::translate(glm::mat4(1.0f), -SledgeVR::vPlayerPos);
 
-	if (g_VR) {
-		glm::mat4 PositionMatrix = glm::translate(glm::mat4(1.0f), -SledgeVR::vPlayerPos);
+	/*
+		FIXME:
+			converting to mat3 then to mat4 is probably way too expensive to be done per frame
+			this needs to be improved
+	*/
+	glm::mat4 mHMDRot = glm::mat4(glm::mat3(SledgeVR::mHMDPose));
+	mHMDRot = glm::rotate(mHMDRot, SledgeVR::fPlayerRotation, glm::vec3(0, 1, 0));
 
-		/*
-			FIXME:
-				converting to mat3 then to mat4 is probably way too expensive to be done per frame
-				this needs to be improved
-		*/
-		glm::mat4 mHMDRot = glm::mat4(glm::mat3(SledgeVR::mHMDPose));
-		mHMDRot = glm::rotate(mHMDRot, SledgeVR::fPlayerRotation, glm::vec3(0, 1, 0));
+	glm::mat4 LeftViewMatrix = mHMDRot * PositionMatrix;
+	glm::mat4 RightViewMatrix = mHMDRot * PositionMatrix;
 
-		glm::mat4 LeftViewMatrix = mHMDRot * PositionMatrix;
-		glm::mat4 RightViewMatrix = mHMDRot * PositionMatrix;
+	/*
+		TO-DO:
+			There's ghosting between the two textures, it might be necessary to render to two different buffers to fix it
+	*/
+	_DrawScene(pRenderer, a2, SledgeVR::iRTHeight, SledgeVR::iRTWidth, &SledgeVR::mProjectionLeftEye, &LeftViewMatrix);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glEnable(GL_TEXTURE_2D);
+	SledgeVR::DrawLeftEye();
 
-		/*
-			TO-DO:
-				There's ghosting between the two textures, it might be necessary to render to two different buffers to fix it
-		*/
-		_DrawScene(pRenderer, a2, SledgeVR::iRTHeight, SledgeVR::iRTWidth, &SledgeVR::mProjectionLeftEye, &LeftViewMatrix);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glEnable(GL_TEXTURE_2D);
-		SledgeVR::DrawLeftEye();
+	g_Renderer->m_StableVpMatrix = g_Renderer->m_OldStableVpMatrix;
 
-		g_Renderer->m_StableVpMatrix = g_Renderer->m_OldStableVpMatrix;
-
-		_DrawScene(pRenderer, a2, SledgeVR::iRTHeight, SledgeVR::iRTWidth, &SledgeVR::mProjectionRightEye, &RightViewMatrix);
-		glBindFramebuffer(GL_FRAMEBUFFER, 0);
-		glEnable(GL_TEXTURE_2D);
-		SledgeVR::DrawRightEye();
-
-		return;
-	}
+	_DrawScene(pRenderer, a2, SledgeVR::iRTHeight, SledgeVR::iRTWidth, &SledgeVR::mProjectionRightEye, &RightViewMatrix);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glEnable(GL_TEXTURE_2D);
+	SledgeVR::DrawRightEye();
 }
 
 void Teardown::Hooks::DrawScene::Hook() {
